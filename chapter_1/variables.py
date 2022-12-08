0a121f98f9d7c9fc8331f3da31c57557e7dfffab
Wyb <elbrodeahre@gmail.com>
2022-12-08 14:40:02 -0300

    Add variables.py to repository

---
+++ chapter_1/variables.py
@@ -0,0 +1,10 @@
+port = 21
+banner = "FreeFloat FTP Server"
+port_list = [21, 22, 80, 110]
+port_open = True
+print("*** Checking for " + banner + " on port " + str(port))
+
+print(type(banner))
+print(type(port))
+print(type(port_list))
+print(type(port_open))
