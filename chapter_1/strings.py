f714c75f2113e60c3d47c3b85ab5f4672acfaca4
Wyb <elbrodeahre@gmail.com>
2022-12-16 15:45:12 -0300

    Add strings.py to repo

---
+++ chapter_1/strings.py
@@ -0,0 +1,14 @@
+banner = "FreeFloat FTP Server"
+
+banner_mayuscula = banner.upper()
+print(banner_mayuscula)
+
+banner_minuscula = banner.lower()
+print(banner_minuscula)
+
+banner_reemplazado = banner.replace('FreeFloat', 'Ivan')
+print(banner_reemplazado)
+
+location_of_ftp = banner.find('FTP')
+print('location of FTP: ' + str(location_of_ftp))
+
