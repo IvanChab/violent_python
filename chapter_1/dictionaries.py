ab8b90cea944a049c50d8ec71613b33cfa99f579
Wyb <elbrodeahre@gmail.com>
2022-12-17 20:09:31 -0300

    Add dictionaries.py to repo

---
+++ chapter_1/dictionaries.py
@@ -0,0 +1,8 @@
+services = {'ftp': 21, 'ssh': 22, 'smtp': 25, 'http': 80}
+services.keys(['ftp', 'smtp', 'ssh', 'http'])
+services.items([('ftp', 21), ('smtp', 25), ('ssh', 22), ('http', 80)])
+services.has_key('ftp')
+True
+services['ftp']
+21
+print("{<'+'>} Found vuln with FTP on port" + str(services['ftp']))
