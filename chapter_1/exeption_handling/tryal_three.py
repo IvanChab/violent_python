49305c1b72c0edead2959c9f2be471b11ea3bd6f
Wyb <elbrodeahre@gmail.com>
2023-01-20 14:37:43 -0300

    Correct errors

--- chapter_1/exeption_handling/tryal_three.py
+++ chapter_1/exeption_handling/tryal_three.py
@@ -1,3 +1,4 @@
+
 try:
     print("{+} 1337/0 = " + str(1337/0))
 except Exception as e:
--- chapter_1/functions/functions_one.py
+++ chapter_1/functions/functions_one.py
@@ -16,11 +16,15 @@ def main() -> object:
     ip1 = 'localhost'
     ip2 = 'localhost'
     port = '2121'
+
     banner1 = ret_banner(ip1, port)
     if banner1:
         print('{+} ' + ip1 + ': ' + banner1)
+
     banner2 = ret_banner(ip2, port)
     if banner2:
         print('{+}' + ip2 + ': ' + banner2)
-        if __name__ == '__main__':
-            main()
+
+
+if __name__ == '__main__':
+    main()
